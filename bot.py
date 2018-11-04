9a20429f8b61521d9ee52bb623db682d5112e7d9
MissNela <44345612+MissNela@users.noreply.github.com>
2018-11-04 15:23:41 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+my_token: 'NTAzNDQyMTI3NjI0MjA4Mzk1.Dq26xg.j7_57wgIbB3Z8nPtyRwWS1_Kv3A'
+
+bot=commands.Bot(command_prefix='D!')
+
+@bot.event
+async def on_ready():
+	print('The bot is ready!')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('----------------------------')
+
+@bot.command()
+async def touch():
+	await bot.say('im gonna touch this')
+@bot.command()
+async def oof():
+	await bot.say('hello')
+
+@bot.command()
+async def hey():
+	await bot.say('wassuh')
+
+@bot.command()
+async def hot():
+	await bot.say('lil pinaple')
+	await bot.say('Dont try it.. You dont have any chance. Heheh')	
+		
+bot.run(my_token)
